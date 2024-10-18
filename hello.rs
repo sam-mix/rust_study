60caa0cd5d6a922d538c5ebd5e06d1051cd05447
x <x@x.x>
2024-10-18 21:56:03 +0800

    x

---
+++ hello.rs
@@ -0,0 +1,13 @@
+fn main() {
+    // Prints the output
+    print!("Hello World\n");
+
+    // Appends a new line after printing
+    println!("Appending a new line");
+
+    // Prints as an error
+    eprint!("This is an error\n");
+
+    // Prints as an error with new line
+    eprintln!("This is an error with new line");
+}
