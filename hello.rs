f6b0fa93efa37d08044fa6ad02c2c969be3399db
x <x@x.x>
2024-10-18 22:00:20 +0800

    xxx

--- hello.rs
+++ hello.rs
@@ -10,4 +10,21 @@ fn main() {
 
     // Prints as an error with new line
     eprintln!("This is an error with new line");
+
+    // Initializing and declaring a variable
+    let some_variable = "This_is_a_variable";
+    println!("{}", some_variable);
+
+    // Making a variable mutable
+    let mutable_variable = "Mutable";
+    // mutable_variable = "c";
+    println!("{}", mutable_variable);
+
+    // Assigning multiple variables
+    let (_name, _age) = ("ElementalX", 20);
+
+    // (Global) constant
+    const SCREAMING_SNAKE_CASE: i64 = 9;
+
+    println!("{}", SCREAMING_SNAKE_CASE);
 }
